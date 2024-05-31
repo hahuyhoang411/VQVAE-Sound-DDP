4b533edaeb7f35369e827ccd1ea3f6962832accf
hahuyhoang411 <hahuyhoanghhh41@gmail.com>
2024-05-31 10:19:13 +0700

    fix path

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 # single_speaker_data_path = '/path/to/single/dataset'
 
 # The path to the directory containing the multi-speaker data set
-multi_speaker_data_path = 'VQVAE/data_processed'
+multi_speaker_data_path = '/data_processed'
